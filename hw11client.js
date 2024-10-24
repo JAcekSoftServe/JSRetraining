5e07993bfadb23b0af263930d656795a762c5f92
JAcekSoftServe <incubus@o2.pl>
2024-10-24 15:55:54 +0300

    hw11_updt01

--- hw11client.js
+++ hw11client.js
@@ -3,7 +3,7 @@
 // onclick().getRequest("hw11server.js").innerHTML("message")
 
 function getMessage() {
-    fetch("localhost:8000").then((result) => result.text())
+    fetch("http://localhost:8000").then((result) => result.text())
                            .then((result) => {
                             document.getElementById("button").innerHTML = result
                         });
